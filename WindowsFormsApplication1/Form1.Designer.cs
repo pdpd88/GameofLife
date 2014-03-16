880059c4afe66feb93ffa6d0378cb1a45e9a367f
Pampos <pdpd88@hotmail.com>
2014-03-16 10:38:25 +0000

    added the project

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(677, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(64, 96);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(510, 434);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(677, 269);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Reset!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(677, 365);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Exit!";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(64, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(510, 37);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Conway\'s Game Of Life";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.optionsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(840, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.resetToolStripMenuItem});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(102, 22);
+            this.resetToolStripMenuItem.Text = "Reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.resetToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGreen;
+            this.ClientSize = new System.Drawing.Size(840, 582);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,462 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        int[,] show = new int[101, 101];
+        int[,] calc = new int[101, 101];
+        int sum = 0;
+        
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+           
+                start(); //fill arrays with 0
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.DoubleBuffered = true;
+           // this.Paint += new PaintEventHandler(Form1_Paint);
+        }
+
+        private void start()
+        {
+            int y = 0;
+            int x = 0;
+            while (x < 100)
+            {
+                while (y < 100)
+                {
+                    show[x, y] = 0;
+                    calc[x, y] = 0;
+                    y++;
+                }
+                y = 0;
+                x++;
+            }
+            x = 0;
+            y = 0;
+            //this.Paint += new PaintEventHandler(Form1_Paint);
+        }
+
+        private void copy()
+        {
+            int y = 0;
+            int x = 0;
+            while (x < 100)
+            {
+                while (y < 100)
+                {
+                    show[x, y] = calc[x, y];
+                    y++;
+                }
+                y = 0;
+                x++;
+            }
+            x = 0;
+            y = 0;
+            Refresh();
+            //this.Paint += new PaintEventHandler(Form1_Paint);
+        }
+
+        #region newgen
+        private void newgeneration()
+        {
+            int y = 0;
+            int x = 0;
+            while (x < 100)
+            {
+                while (y < 100)
+                {
+                    if ((y == 0) && (x == 0))   // Calculate Top left
+                    {
+                        if (show[x + 1, y] + show[x + 1, y + 1] + show[x, y + 1] == 3)
+                        {
+                            calc[x, y] = 1;
+                        }
+                        else
+                        {
+                            calc[x, y] = 0;
+                        }
+                    }
+
+                    if ((y == 0) && (x == 100))   // Calculate Top right
+                    {
+                        if (show[x - 1, y] + show[x - 1, y + 1] + show[x, y + 1] == 3)
+                        {
+                            calc[x, y] = 1;
+                        }
+                        else
+                        {
+                            calc[x, y] = 0;
+                        }
+                    }
+
+                    if ((y == 100) && (x == 0))   // Calculate Bottom Left
+                    {
+                        if (show[x, y - 1] + show[x + 1, y - 1] + show[x + 1, y] == 3)
+                        {
+                            calc[x, y] = 1;
+                        }
+                        else
+                        {
+                            calc[x, y] = 0;
+                        }
+                    }
+
+                    if ((y == 100) && (x == 100)) // Calculate Bottom Right
+                    {
+                        if (show[x, y - 1] + show[x - 1, y - 1] + show[x - 1, y] == 3)
+                        {
+                            calc[x, y] = 1;
+                        }
+                        else
+                        {
+                            calc[x, y] = 0;
+                        }
+                    }
+
+                    if ((y == 0) && (x > 0) && (x < 100)) //Calculate Top
+                    {
+                        //int sum = 0;
+                        sum = show[x - 1, y] + show[x - 1, y + 1] + show[x, y + 1] + show[x + 1, y + 1] + show[x + 1, y];
+
+                        if ((show[x, y] == 0) && (sum == 3))
+                        {
+                            calc[x, y] = 1;
+                        }
+
+                        if ((show[x, y] == 1) && (sum <= 1))
+                        {
+                            calc[x, y] = 0;
+                        }
+
+                        if ((show[x, y] == 1) && (sum >= 4))
+                        {
+                            calc[x, y] = 0;
+                        }
+
+                        if ((show[x, y] == 1) && ((sum >= 2) && (sum <= 3)))
+                        {
+                            calc[x, y] = 1;
+                        }
+
+                        sum = 0;
+                    }
+
+                    if ((x == 0) && (y != 0) && (y != 100)) //Calculate Left
+                    {
+                        //int sum = 0;
+                        sum = show[x, y - 1] + show[x + 1, y - 1] + show[x + 1, y] + show[x + 1, y + 1] + show[x, y + 1] + 1;
+
+                        if ((show[x, y] == 0) && (sum == 3))
+                        {
+                            calc[x, y] = 1;
+                        }
+
+                        if ((show[x, y] == 1) && (sum <= 1))
+                        {
+                            calc[x, y] = 0;
+                        }
+
+                        if ((show[x, y] == 1) && (sum >= 4))
+                        {
+                            calc[x, y] = 0;
+                        }
+
+                        if ((show[x, y] == 1) && ((sum >= 2) && (sum <= 3)))
+                        {
+                            calc[x, y] = 1;
+                        }
+
+                        sum = 0;
+                    }
+
+                    if ((x == 100) && (y != 0) && (y != 100)) //Calculate Right
+                    {
+                        //int sum = 0;
+                        sum = show[x, y - 1] + show[x - 1, y - 1] + show[x - 1, y] + show[x - 1, y + 1] + show[x, y + 1];
+
+                        if ((show[x, y] == 0) && (sum == 3))
+                        {
+                            calc[x, y] = 1;
+                        }
+
+                        if ((show[x, y] == 1) && (sum <= 1))
+                        {
+                            calc[x, y] = 0;
+                        }
+
+                        if ((show[x, y] == 1) && (sum >= 4))
+                        {
+                            calc[x, y] = 0;
+                        }
+
+                        if ((show[x, y] == 1) && ((sum >= 2) && (sum <= 3)))
+                        {
+                            calc[x, y] = 1;
+                        }
+
+                        sum = 0;
+                    }
+
+                    if ((y == 100) && (x != 0) && (x != 100)) //Calculate Bottom
+                    {
+                        //int sum = 0;
+                        sum = show[x - 1, y] + show[x - 1, y - 1] + show[x, y - 1] + show[x + 1, y - 1] + show[x + 1, y];
+
+                        if ((show[x, y] == 0) && (sum == 3))
+                        {
+                            calc[x, y] = 1;
+                        }
+
+                        if ((show[x, y] == 1) && (sum <= 1))
+                        {
+                            calc[x, y] = 0;
+                        }
+
+                        if ((show[x, y] == 1) && (sum >= 4))
+                        {
+                            calc[x, y] = 0;
+                        }
+
+                        if ((show[x, y] == 1) && ((sum >= 2) && (sum <= 3)))
+                        {
+                            calc[x, y] = 1;
+                        }
+
+                        sum = 0;
+                    }
+
+                    if ((x != 0) && (y != 0) && (x != 100) && (y != 100)) //Calculate Center
+                    {
+                        //int sum = 0;
+                        sum = show[x - 1, y - 1] + show[x, y - 1] + show[x + 1, y - 1] + show[x - 1, y] + show[x + 1, y] + show[x - 1, y + 1] + show[x, y + 1] + show[x + 1, y + 1];
+
+                        if ((show[x, y] == 0) && (sum == 3))
+                        {
+                            calc[x, y] = 1;
+                        }
+
+                        if ((show[x, y] == 1) && (sum <= 1))
+                        {
+                            calc[x, y] = 0;
+                        }
+
+                        if ((show[x, y] == 1) && (sum >= 4))
+                        {
+                            calc[x, y] = 0;
+                        }
+
+                        if ((show[x, y] == 1) && ((sum >= 2) && (sum <= 3)))
+                        {
+                            calc[x, y] = 1;
+                        }
+
+                        sum = 0;
+                    }
+
+                    y++;
+                }
+                y = 0;
+                x++;
+            }
+            x = 0;
+            y = 0;
+            copy();
+            //this.Paint += new PaintEventHandler(Form1_Paint);
+        }
+        #endregion
+
+        /*    private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            int y = 0;
+            int x = 0;
+            int a = 15;
+            while (y < 100)
+            {
+                while (x < 100)
+                {
+                    if (show[x, y] == 1)
+                    {
+                        Brush brush = new SolidBrush(Color.Yellow);
+                        g.FillRectangle(brush, x * a + a, y * a + a, 10, 10);
+                    }
+                    else
+                    {
+                        Brush brush = new SolidBrush(Color.Black);
+                        g.FillRectangle(brush, x * a + a, y * a + a, 10, 10);
+                    }
+                    x++;
+                }
+                x = 0;
+                y++;
+            }
+            x = 0;
+            y = 0;
+        }*/
+        int starts = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //start from button
+            if (starts == 1)
+            {
+                starts = 0;
+            }
+            else
+            {
+                starts = 1;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //if button is clicked, start newgeneration method   
+            if (starts == 1)
+            {
+                newgeneration();
+            }
+            else { Refresh(); }
+        }
+
+      /*  private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            int xe = e.X;
+            int ye = e.Y;
+            int a = 15; 
+            show[(xe - a) / a, (ye - a) / a] = 1;
+            calc[(xe - a) / a, (ye - a) / a] = 1;
+        }*/
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            //paint in picturebox
+            Graphics g = e.Graphics;
+            int y = 0;
+            int x = 0;
+            int a = 15;
+            while (y < 100)
+            {
+                while (x < 100)
+                {
+                    if (show[x, y] == 1)
+                    {
+                        Brush brush = new SolidBrush(Color.Green);
+                        g.FillRectangle(brush, x * a + a, y * a + a, 10, 10);
+                    }
+                    else
+                    {
+                        Brush brush = new SolidBrush(Color.Red);
+                        g.FillRectangle(brush, x * a + a, y * a + a, 10, 10);
+                    }
+                    x++;
+                }
+                x = 0;
+                y++;
+            }
+            x = 0;
+            y = 0;
+        }
+
+        int clicks = 0;
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            clicks = 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //reset from button
+          start();
+          starts = 0;    
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show("Thank you come back soon! :)");
+            this.Close(); // close from button
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //start from menu
+            if (starts == 1)
+            {
+                starts = 0;
+            }
+            else
+            {
+                starts = 1;
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show("Thank you come back soon! :)");
+            this.Close(); //close from menu
+        }
+
+        private void resetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //reset from menu
+            start();
+            starts = 0;  
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show("Click on some dots and then start to see the game! Enjoy!");
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            clicks = 0;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int xe = e.X;
+            int ye = e.Y;
+            int a = 15;
+            if (xe < 606 && xe > 6 && ye < 606 && ye > 6 && clicks == 1)
+            {
+                if (e.Button == MouseButtons.Right) // | Right Click > Cell Die |
+                {
+                    show[(xe - a) / a, (ye - a) / a] = 0;
+                    calc[(xe - a) / a, (ye - a) / a] = 0;
+                }
+                else    // | Left Click > Cell Alive |
+                {
+                    show[(xe - a) / a, (ye - a) / a] = 1;
+                    calc[(xe - a) / a, (ye - a) / a] = 1;
+                }
+            }
+        }
+
+       
+
+        }
+
+       
+
+
+
+
+    }
+
